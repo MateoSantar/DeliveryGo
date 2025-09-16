fad3425888aa16439b0b75e17c5ae5575aa49f65
Titi <104162319+Thiago20035@users.noreply.github.com>
2025-09-16 18:44:33 -0300

    ConfigManager clase

---
+++ TrabajoIntegralProga/classes/ConfigManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public sealed class ConfigManager
+{
+    public ConfigManager()
+    {
+        // Singleton de config (Etapa 2) 
+        private static readonly Lazy<ConfigManager>
+        _inst = new(() => new ConfigManager());
+    public static ConfigManager Instance => _inst.Value;
+    private ConfigManager() { }
+    public decimal EnvioGratisDesde { get; set; } = 50000m;
+    public decimal IVA { get; set; } = 0.21m; // usado por decorator en Etapa 3
+}
+}
