7a5cc770d18fe6505493024a715a05f5ba938302
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-09-16 18:47:23 -0300

    ConfigManager Fix

--- TrabajoIntegralProga/classes/ConfigManager.cs
+++ TrabajoIntegralProga/classes/ConfigManager.cs
@@ -2,14 +2,11 @@
 
 public sealed class ConfigManager
 {
-    public ConfigManager()
-    {
-        // Singleton de config (Etapa 2) 
-        private static readonly Lazy<ConfigManager>
-        _inst = new(() => new ConfigManager());
+
+    private static readonly Lazy<ConfigManager> _inst = new(() => new ConfigManager());
     public static ConfigManager Instance => _inst.Value;
     private ConfigManager() { }
     public decimal EnvioGratisDesde { get; set; } = 50000m;
     public decimal IVA { get; set; } = 0.21m; // usado por decorator en Etapa 3
 }
-}
+
