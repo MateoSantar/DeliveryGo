9e82385260fb461a998915921be8117ba3ba1afb
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-09-17 00:38:58 -0300

    Main Strategy Example Added

--- TrabajoIntegralProga/Program.cs
+++ TrabajoIntegralProga/Program.cs
@@ -1,9 +1,19 @@
 ﻿namespace TrabajoIntegralProga;
-
+using classes;
+using global::classes.Core.Strategy;
+using interfaces;
 class Program
 {
     static void Main(string[] args)
     {
         ConfigManager.Instance.EnvioGratisDesde = 50000m;
+        EnvioService envioService = new EnvioService(new EnvioCorreo());
+        Console.WriteLine($"{envioService.NombreActual()}: {envioService.Calcular(60000m)}");
+        envioService.SetStrategy(new EnvioMoto());
+        Console.WriteLine($"{envioService.NombreActual()}: {envioService.Calcular(60000m)}");
+        envioService.SetStrategy(new RetiroEnTienda());
+        Console.WriteLine($"{envioService.NombreActual()}: {envioService.Calcular(60000m)}");
+
+
     }
 }
