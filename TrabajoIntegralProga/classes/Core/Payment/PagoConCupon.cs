9c1ab0640d8bfedaaf03d0b7e8170c887448e918
pedro Mendoza <pedrojoaquinmendoza@gmail.com>
2025-09-23 12:51:16 -0300

    feat: Decorators hechos, falta llamarlos cuando se cree el objeto, la logica donde se llamen.

--- TrabajoIntegralProga/classes/Core/Payment/PagoConCupon.cs
+++ TrabajoIntegralProga/classes/Core/Payment/PagoConCupon.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using enums;
+using interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace classes.Core.Payment
 {
-    class PagoConCupon
+    class PagoConCupon : IPago
     {
+        private IPago _inner;
+        private decimal _porcentaje = 0.10m; 
+
+        public PagoConCupon(IPago inner, decimal porcentaje)
+        {
+            this._inner = inner;
+            this._porcentaje = porcentaje;
+        }
+
+        public PagoNombre Nombre => _inner.Nombre;
+
+        public bool Procesar(decimal monto)
+        {
+            var total = monto * (1 - _porcentaje);
+            return _inner.Procesar(total);
+        }
     }
 }
--- TrabajoIntegralProga/classes/Core/Payment/PagoConImpuesto.cs
+++ TrabajoIntegralProga/classes/Core/Payment/PagoConImpuesto.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using enums;
+using interfaces;
 
 namespace classes.Core.Payment
 {
-    class PagoConImpuesto
+    class PagoConImpuesto : IPago
     {
+        private IPago inner;
+
+        public PagoConImpuesto(IPago inner)
+        {
+            this.inner = inner;
+        }
+
+        public PagoNombre Nombre => inner.Nombre;
+
+        public bool Procesar(decimal monto)
+        {
+            var total = monto * (1 + ConfigManager.Instance.IVA);
+            return inner.Procesar(total);
+        }
     }
 }
