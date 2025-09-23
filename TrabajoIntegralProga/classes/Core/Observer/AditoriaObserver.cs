28be16a8b5bbd4ebbc8812f668857ed6c716d08e
pedro Mendoza <pedrojoaquinmendoza@gmail.com>
2025-09-23 16:42:36 -0300

    feat: Observer hecho

--- TrabajoIntegralProga/classes/Core/Observer/AditoriaObserver.cs
+++ TrabajoIntegralProga/classes/Core/Observer/AditoriaObserver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using classes.Core.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace classes.Core.Observer
 {
     class AditoriaObserver
     {
+        public void Suscribir(PedidoService p) => p.EstadoCambiado += OnEstadoCambiado;
+        public void Desuscribir(PedidoService p) => p.EstadoCambiado -= OnEstadoCambiado;
+        private void OnEstadoCambiado(object? sender, PedidoChangedEventArgs p)
+        {
+            Console.WriteLine($"[AUDITORIA] Pedido {p.PedidoId} cambiado a {p.NuevoEstado} en {DateTime.Now}");
+        }
     }
 }
--- TrabajoIntegralProga/classes/Core/Observer/LogisticaObserver.cs
+++ TrabajoIntegralProga/classes/Core/Observer/LogisticaObserver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using classes.Core.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace classes.Core.Observer
 {
     class LogisticaObserver
     {
+        public void Suscribir(PedidoService p) => p.EstadoCambiado += OnEstadoCambiado;
+        public void Desuscribir(PedidoService p) => p.EstadoCambiado -= OnEstadoCambiado;
+        private void OnEstadoCambiado(object? sender, PedidoChangedEventArgs p)
+        {
+            if (p.NuevoEstado == enums.EstadoPedido.Enviado)
+            {
+                Console.WriteLine($"[LOGISTICA] Pedido {p.PedidoId} ha sido enviado. Preparando para la entrega.");
+            }
+            else
+            {
+                Console.WriteLine($"[LOGISTICA] Pedido {p.PedidoId} => Estado: {p.NuevoEstado}");
+            }
+        }
     }
 }
